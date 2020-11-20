e33242fd9df6a2a4f2b304833566b28af6bc2dcb
sei1002 <sei1002210@gmail.com>
2020-11-20 17:25:55 +0900

    Update app.js

--- app.js
+++ app.js
@@ -10,16 +10,16 @@ const correct = 'メッシ';
 //定数の文字列をHTMLに反映させる
 document.getElementById('js-question').textContent = question;
 
-const button = document.getElementsByTagName('button');
+const $button = document.getElementsByTagName('button');
 
-button[0].textContent = answers[0];
-button[1].textContent = answers[1];
-button[2].textContent = answers[2];
-button[3].textContent = answers[3];
+$button[0].textContent = answers[0];
+$button[1].textContent = answers[1];
+$button[2].textContent = answers[2];
+$button[3].textContent = answers[3];
 
 //ボタンをクリックしたら正誤判定
-button[0].addEventListener('click', () => {
-  if(correct === document.getElementsByTagName('button')[0].textContent){
+$button[0].addEventListener('click', () => {
+  if(correct === $button[0].textContent){
     window.alert('正解！');
   }else {
     window.alert('不正解！');

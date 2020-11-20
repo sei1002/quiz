fce8c48e22c75528893390b242410d2ce4edd3fa
sei1002 <sei1002210@gmail.com>
2020-11-20 17:23:25 +0900

    答えの値を定数に設定

--- app.js
+++ app.js
@@ -7,15 +7,18 @@ const answers = [
 ];
 const correct = 'メッシ';
 
+//定数の文字列をHTMLに反映させる
 document.getElementById('js-question').textContent = question;
 
-document.getElementsByTagName('button')[0].textContent = answers[0];
-document.getElementsByTagName('button')[1].textContent = answers[1];
-document.getElementsByTagName('button')[2].textContent = answers[2];
-document.getElementsByTagName('button')[3].textContent = answers[3];
+const button = document.getElementsByTagName('button');
+
+button[0].textContent = answers[0];
+button[1].textContent = answers[1];
+button[2].textContent = answers[2];
+button[3].textContent = answers[3];
 
 //ボタンをクリックしたら正誤判定
-document.getElementsByTagName('button')[0].addEventListener('click', () => {
+button[0].addEventListener('click', () => {
   if(correct === document.getElementsByTagName('button')[0].textContent){
     window.alert('正解！');
   }else {
